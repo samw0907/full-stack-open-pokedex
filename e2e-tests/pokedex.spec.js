2318d0cf54acb6bcbd5b834406e19020ef3203ab
Sam <swilliamson_0907@outlook.com>
2024-12-04 17:36:20 +0200

    added test for directing to ivysaur page

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-})
+
+  test('can navigate to ivysaur page and see its abilities', async ({ page }) => {
+    await page.goto('/');
+    await page.click('text=ivysaur');
+    await expect(page.getByText('chlorophyll')).toBeVisible();
+    await expect(page.getByText('ivysaur')).toBeVisible();
+  });
+});
